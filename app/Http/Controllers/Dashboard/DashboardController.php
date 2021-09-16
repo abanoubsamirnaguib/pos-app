8d9af48c3d3ebac4f8cb6953134127fc0bdc237e
unknown <abanoubsamir@gmail.com>
2021-09-16 02:22:35 +0200

    f

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -21,20 +21,28 @@ class DashboardController extends Controller
         $products_count=product::all()->count();
         $users_count=User::whereRoleIs()->count();
 
-        // $sales_data = order::
-        //         selectRaw('YEAR(created_at) as year')->
-        //         selectRaw('MONTH(created_at) as month')->
-        //         selectRaw('SUM(total_price) as sum')
-        //         ->groupBy( 'year' ,'month' )
-        //         ->get();
-
+        $sales_data1 = order::
+                selectRaw('created_at as year')->
+                selectRaw('(created_at) as month')->
+                selectRaw('SUM(total_price) as sum')
+                ->groupBy( 'year' ,'month' )
+                ->get();
+                
+                $sales_data =[];
+                foreach ($sales_data1 as $i=>$data){
+                $sales_data [$i] = [
+                    "year" => date('Y', strtotime($data->year)),
+                    "month" => date('m', strtotime($data->month)),
+                    "sum" => $data->sum
+                ];
+            };
         // dd( $sales_data );
         return view("dashboard.welcome",
         ["categories_count"=>$categories_count,
         "products_count"=>$products_count ,
         "clients_count"=>$clients_count,
         "users_count"=>$users_count,
-        // 'sales_data'=>$sales_data
+        'sales_data'=>$sales_data
     ]);
     }
 }
